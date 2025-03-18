6909b1f1630b597e8a96f7e0ac7696681b9878ee
usman <muhammad.usman@infinitytech.gg>
2025-03-18 20:44:48 +0500

    Your commit message

--- resources/views/docker/services.blade.php
+++ resources/views/docker/services.blade.php
@@ -25,6 +25,10 @@
                 @csrf
                 <button type="submit" class="btn btn-success">Start Engine</button>
             </form>
+            <form action="{{ route('stop_engine') }}" method="POST">
+                @csrf
+                <button type="submit" class="btn btn-danger">Stop Engine</button>
+            </form>
 
             @if($services)
                 <div class="row mt-5">
